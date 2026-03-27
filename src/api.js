099711a3ea91b5c58670e8d06346b6eaf7d0a28e
lakshay-chauhan <lakshay_chauhan@outlook.com>
2026-03-27 12:20:00 +0530

    changes

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://green-cloud-backend.onrender.com", // change after deployment
+  baseURL: "https://green-cloud-backend.onrender.com/run-simulation", // change after deployment
 });
 
 export const runSimulation = async () => {
